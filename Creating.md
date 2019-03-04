bc1b5da01fb4873587f50c865e395ecb935ee256
Jason Goemaat <jason.goemaat@gmail.com>
2019-03-03 20:26:06 -0600

    3

--- Creating.md
+++ Creating.md
@@ -2,5 +2,20 @@ Commands used to create this project:
 
     ng new extension --style=scss --routing
     cd extension
+
+### commit '1'
+
     ng g library extension-common --prefix=lib --style=scss
-    
+
+### '2'
+
+    yarn add @angular/material @angular/cdk @angular/animations
+
+Followed [getting-started](https://material.angular.io/guide/getting-started)
+to add it to the app.  See [the source](https://github.com/angular/material2/tree/master/src/material-examples)
+for their examples for usage.
+
+    yarn add font-awesome
+
+### '3'
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatIconModule } from "@angular/material";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapabilitiesComponent } from './capabilities/capabilities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapabilitiesComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
